a5c6fa52098d67eed8934fc1152dd8fa9154f0c0
Kirill Melnikov <dezxplay@gmail.com>
2025-02-26 15:03:13 +0200

    Update Oigused.py

--- Oigused.py
+++ Oigused.py
@@ -17,3 +17,28 @@ privacyData = {
 with open("privacy_template.json", "w", encoding="utf-8") as file:
   json.dump(privacyData,file, indent=4)
 print("kõik oli edukas salvestastud")
+
+html_template = """
+<!DOCTYPE html>
+<html>
+<head>
+<title> Privaatsuspoliitika </title> 
+</head>
+<body>
+  <h1>Poliitika pühendanud ettevõttele - "{companyName}"</h1>
+  <p>Kontakt: {contactEmail}</p>
+  
+  <h2>Millised andmed kogume?</h2>
+  <p>{dataCollectionType}</p>
+
+  <h2>Kuidas andmed kasutatakse?</h2>
+  <p>{dataUsage}</p>
+  
+  <h2>Kui kaua salvestame?</h2>
+  <p>{dataStorageLimit}</p>
+  
+</body>
+</html>
+ """
+
+privacy_policy = html_template.format(privacyData)
